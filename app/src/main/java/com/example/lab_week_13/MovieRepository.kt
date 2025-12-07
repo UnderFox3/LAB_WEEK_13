4b59ff2599e7b1c5252b3b0964d2dd5e446af27e
Activity_Lifecycle <natan.aw95@gmail.com>
2025-12-07 18:41:44 +0700

    Commit No. 1 - Add data binding

---
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -0,0 +1,27 @@
+package com.example.lab_week_13
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(private val movieService: MovieService) {
+    private val apiKey = "3583e371e4807307e6f0480a98db2eb3"
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
+    }
+}
---
+++ app/src/main/java/com/example/lab_week_13/MovieViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.lab_week_13
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.lab_week_13.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+
+class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+    init {
+        fetchPopularMovies()
+    }
+
+    private val _popularMovies = MutableStateFlow(
+        emptyList<Movie>()
+    )
+
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies().catch {
+                _error.value = "An exception occurred: ${it.message}"
+            }.collect { movieList ->
+                val filtered = movieList.sortedByDescending { it.popularity }
+
+                _popularMovies.value = filtered
+            }
+        }
+    }
+}
